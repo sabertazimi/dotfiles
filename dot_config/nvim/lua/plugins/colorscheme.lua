e606208d605464c994de783f970e3b5dada2c6e2
sabertazimi <sabertazimi@gmail.com>
2026-03-15 20:12:26 +0800

    feat(nvim): switch to the official `catppuccin` color scheme

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,8 @@
 return {
   {
-    "RRethy/base16-nvim",
-    priority = 1000,
-  },
-  {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "base16-catppuccin-mocha",
+      colorscheme = "catppuccin-mocha",
     },
   },
 }
