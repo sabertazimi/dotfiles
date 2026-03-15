b6c7ec7c7ae13fc4b5c4baf7429986980a692125
sabertazimi <sabertazimi@gmail.com>
2026-03-15 20:13:50 +0800

    refactor(nvim-lualine): simplify separator configuration
    Remove custom capsule function in favor of direct option setting.

--- dot_config/nvim/lua/plugins/lualine.lua
+++ dot_config/nvim/lua/plugins/lualine.lua
@@ -1,26 +1,11 @@
 return {
   {
     "nvim-lualine/lualine.nvim",
-    opts = function(_, opts)
-      opts.options = opts.options or {}
-      opts.options.component_separators = ""
-      opts.options.section_separators = { left = "", right = "" }
-      opts.sections = opts.sections or {}
-
-      local function apply_capsule(section)
-        if not section or not section[1] then
-          return
-        end
-
-        if type(section[1]) == "table" then
-          section[1].separator = { left = "", right = "" }
-        else
-          section[1] = { section[1], separator = { left = "", right = "" } }
-        end
-      end
-
-      apply_capsule(opts.sections.lualine_a)
-      apply_capsule(opts.sections.lualine_z)
-    end,
+    opts = {
+      options = {
+        component_separators = "",
+        section_separators = { left = "", right = "" },
+      },
+    },
   },
 }
