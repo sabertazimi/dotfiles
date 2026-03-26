2e3731ed66408851333b346b2ddd4db06e18cf17
sabertazimi <sabertazimi@gmail.com>
2026-03-26 13:55:27 +0800

    feat(nvim): enable image rendering in documents

--- dot_config/nvim/lua/plugins/snacks.lua
+++ dot_config/nvim/lua/plugins/snacks.lua
@@ -1,8 +1,10 @@
 return {
   {
     "folke/snacks.nvim",
+    ---@type snacks.Config
     opts = {
       scroll = { enabled = false }, -- disable smooth scrolling
+      image = { doc = { enabled = true } }, -- enable image rendering
     },
   },
 }
