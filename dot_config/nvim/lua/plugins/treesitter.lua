17896059398e9b151d78be8553a66642dc308946
sabertazimi <sabertazimi@gmail.com>
2026-04-02 02:46:45 +0800

    feat(nvim): set up Java tree sitter

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,6 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    opts = { ensure_installed = { "bibtex", "latex" } },
+    opts = { ensure_installed = { "java", "bibtex", "latex" } },
   },
 }
