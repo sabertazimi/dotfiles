922ab94d62463804d45d9a1a7737666a8139c444
sabertazimi <sabertazimi@gmail.com>
2026-04-26 12:00:09 +0800

    feat(nvim): enable `catppuccin` with transparent background

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,5 +1,15 @@
 return {
   {
+    "catppuccin/nvim",
+    opts = {
+      transparent_background = true,
+      float = {
+        transparent = true,
+        solid = true,
+      },
+    },
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "catppuccin-mocha",
